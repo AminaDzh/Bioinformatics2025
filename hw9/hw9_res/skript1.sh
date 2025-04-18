c2857d2c9930645071ad210dbcbc763befe8c154
Amina Dzhioeva <adzhioeva@frontend-2-2-13.int.aldan3.itm-rsmu.ru>
2025-04-18 13:46:37 +0300

    added hw9

---
+++ hw9/hw9_res/skript1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/082/ERR14230582/ERR14230582.fastq.gz -o ERR14230582_Illumina_HiSeq_4000_sequencing.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/007/ERR14230607/ERR14230607.fastq.gz -o ERR14230607_Illumina_HiSeq_4000_sequencing.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/086/ERR14230586/ERR14230586.fastq.gz -o ERR14230586_Illumina_HiSeq_4000_sequencing.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/000/ERR14230600/ERR14230600.fastq.gz -o ERR14230600_Illumina_HiSeq_4000_sequencing.fastq.gz
